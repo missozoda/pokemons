29778f265d0baa9ee739dc849b3762ccfc46315c
missozoda <ozoshka1207@gmail.com>
2022-05-11 16:46:13 +0500

    add changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 let resultList = $(".list");
 
 for (let i = 0; i < pokemons.length; i++){
-  let newLi = createElement("li", "card col-3");
+  let newLi = createElement("li", "card col-3 p-1 border-1 border-warning rounded-0");
   let newImg = createElement("img", "card-img-top");
   newImg.setAttribute("src", pokemons[i].img);
   let newDiv = createElement("div", "card-body");
-  let newH = createElement("h5", "card-title", pokemons[i].name);
-  let newP = createElement("p", "card-text");
+  let newH = createElement("h5", "card-title text-danger", `Name: ${pokemons[i].name}`);
+  let newP = createElement("p", "card-text text-success", "Weaknesses: ");
 
   let weaknesses = pokemons[i].weaknesses
   for (let j = 0; j < weaknesses.length; j++){
-    newP.append("  "," • ", weaknesses[j], "  ");
+    let newSpan = createElement("span", "", `  • ${weaknesses[j]}`);
+    newP.append(newSpan);
   }
   newDiv.append(newH, newP);
   newLi.append(newImg, newDiv);
